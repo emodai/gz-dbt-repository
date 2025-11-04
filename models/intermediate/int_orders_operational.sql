fec6730037510f1e767cef1b71587c03f450b19a
modai.eva@gmail.com <modai.eva@gmail.com>
2025-11-04 17:03:59 +0000

    commit4.3

---
+++ models/intermediate/int_orders_operational.sql
@@ -0,0 +1,11 @@
+SELECT orders_id,
+date_date,
+ROUND(margin + shipping_fee - logcost - ship_cost,2) AS operational_margin,
+margin,
+shipping_fee,
+logcost,
+ship_cost
+FROM {{ref('int_orders_margin')}}
+JOIN {{ref('stg_raw__ship')}}
+USING(orders_id)
+ORDER BY date_date DESC
