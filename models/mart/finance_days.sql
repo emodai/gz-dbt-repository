115f3e40f40cf4a41b3b1b1f56eb16083e97d2a2
modai.eva@gmail.com <modai.eva@gmail.com>
2025-11-04 17:36:06 +0000

    finance_days_v.clean

--- models/mart/finance_days.sql
+++ models/mart/finance_days.sql
@@ -1,12 +1,30 @@
-SELECT date_date,
-COUNT(orders_id) AS total_transactions,
-ROUND(SUM(revenue),2) AS total_revenue,
-ROUND(SAFE_DIVIDE(SUM(revenue), COUNT(DISTINCT orders_id)),2) AS average_basket,
-ROUND(SUM(operational_margin),2) AS total_operational_margin,
-ROUND(SUM(purchase_cost),2) AS total_purchase_cost,
-ROUND(SUM(shipping_fee),2) AS total_shipping_fees,
-ROUND(SUM(logcost),2) AS total_log_costs,
-ROUND(SUM(quantity),2) AS total_quantity
-FROM {{ref('int_orders_operational')}}
-GROUP BY date_date
-ORDER BY date_date DESC
+ -- finance_days.sql
+ WITH orders_per_day AS (
+   SELECT
+     date_date
+     ,COUNT(DISTINCT orders_id) AS nb_transactions
+     ,ROUND(SUM(revenue),0) AS revenue
+     ,ROUND(SUM(margin),0) AS margin
+     ,ROUND(SUM(operational_margin),0) AS operational_margin
+     ,ROUND(SUM(purchase_cost),0) AS purchase_cost
+     ,ROUND(SUM(shipping_fee),0) AS shipping_fee
+     ,ROUND(SUM(logcost),0) AS log_cost
+     ,ROUND(SUM(ship_cost),0) AS ship_cost
+     ,SUM(quantity) AS quantity
+ FROM {{ref("int_orders_operational")}}
+ GROUP BY  date_date
+ )
+
+ SELECT
+     date_date
+     , revenue
+     , margin
+     , operational_margin
+     , purchase_cost
+     , shipping_fee
+     , log_cost
+     , ship_cost
+     , quantity
+     , ROUND(revenue/NULLIF(nb_transactions, 0), 2) AS average_basket
+ FROM orders_per_day
+ ORDER BY  date_date DESC
