e212b4b446112c796a058604abb2c127e91254e7
Eva Modai <modai.eva@gmail.com>
2025-11-05 13:37:12 +0000

    create_int_campaigns_day

--- models/intermediate/int_campaigns_day.sql
+++ models/intermediate/int_campaigns_day.sql
@@ -1,2 +1,8 @@
-SELECT *
+SELECT date_date,
+COUNT(*) AS total_campaigns,
+ROUND(SUM(ads_cost),2) AS total_cost,
+ROUND(SUM(impression),2) AS total_impressions,
+ROUND(SUM(click),2) AS total_clicks
 FROM {{ref('int_campaigns')}}
+GROUP BY date_date
+ORDER BY date_date DESC
--- models/staging/raw/stg_raw__bing.sql
+++ models/staging/raw/stg_raw__bing.sql
@@ -13,7 +13,7 @@ renamed as (
         paid_source,
         campaign_key,
         campgn_name,
-        ads_cost,
+        CAST(ads_cost AS FLOAT64) AS ads_cost,
         impression,
         click,
         CONCAT(date_date, '-', campaign_key) AS unique_key
